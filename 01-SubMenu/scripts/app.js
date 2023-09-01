3c78e8b9601fed0b6f372d8a38d5dd36101902be
aliasadi78 <4.aliasadi.4@gmail.com>
2023-09-01 19:18:35 +0330

    01

---
+++ 01-SubMenu/scripts/app.js
@@ -0,0 +1,7 @@
+const profile = document.querySelector('.img')
+const submenu = document.querySelector('.submenu')
+
+profile.addEventListener('click', () => {
+    submenu.classList.toggle('active')
+})
+
